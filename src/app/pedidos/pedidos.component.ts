3b834aa985191766c8520192dd3b6798f45c72fb
Leonardo Oliveira <64879628+ydragonz@users.noreply.github.com>
2023-11-28 22:52:35 -0300

    Updated order update function

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,4 +1,3 @@
-// pedidos.component.ts
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,21 +6,33 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './pedidos.component.html',
   styleUrls: ['./pedidos.component.css']
 })
-export class PedidosComponent implements OnInit {
+export class PedidosComponent {
   orders: any[] = [];
   mensagemSucesso: string = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.carregarPedidos();
+  }
 
-  ngOnInit() {
-    this.http.get<any[]>('http://localhost:3001/api/orders').subscribe((data: any[]) => {
-      this.orders = data;
+  carregarPedidos() {
+    this.http.get<any[]>('http://localhost:3001/api/orders').subscribe((orders) => {
+      this.orders = orders;
     });
   }
 
   alterarStatusPedido(order: any) {
-    order.status = 'Pronto';
-    this.http.put(`http://localhost:3001/api/orders/${order._id}`, order).subscribe(() => {
+    let novoStatus = '';
+
+    if(order.status === 'Confirmando') {
+      novoStatus = 'Preparando';
+    } else if(order.status === 'Preparando') {
+      novoStatus = 'Pronto';
+    }
+
+    const updatedOrder = { ...order, status: novoStatus };
+
+    this.http.put(`http://localhost:3001/api/orders/${order._id}`, updatedOrder).subscribe(() => {
+      this.carregarPedidos();
       this.mensagemSucesso = 'Status do pedido alterado com sucesso!';
     });
   }
